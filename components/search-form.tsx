8452ffb720763522ada33677c6b227b19c37f2e9
Pranav Kafle <pranavkafle@gmail.com>
2025-04-26 15:06:47 +0530

    feat: implement client-side sidebar search filter

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Search } from "lucide-react"
 
 import { Label } from "@/components/ui/label"
@@ -7,7 +8,13 @@ import {
   SidebarInput,
 } from "@/components/ui/sidebar"
 
-export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+// Define props interface to include value and onChange for the input
+interface SearchFormProps extends Omit<React.ComponentProps<"form">, 'onChange'> {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function SearchForm({ value, onChange, ...props }: SearchFormProps) {
   return (
     <form {...props}>
       <SidebarGroup className="py-0">
@@ -19,6 +26,8 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
             id="search"
             placeholder="Search history..."
             className="pl-8"
+            value={value}
+            onChange={onChange}
           />
           <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
         </SidebarGroupContent>
